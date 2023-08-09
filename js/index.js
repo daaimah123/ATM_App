1b124780e6aa08e153d51255831a2ee537cfd97e
Daaimah Tibrey <daaimahtibrey@Daaimahs-Laptop.hsd1.ca.comcast.net>
2023-08-09 16:15:21 -0700

    hidden behavior

--- js/index.js
+++ js/index.js
@@ -1,31 +1,37 @@
 let balance = 80000;
 
 window.onload = function(){
-    document.getElementById("amountDiv").style.visibility = "hidden";
-    document.getElementById("withdrawalbutton").style.visibility = "hidden";
+    document.getElementById("withdraw").style.visibility = "hidden";
+    document.getElementById("selection").style.visibility = "hidden";
 }
 
 function authenticatedUser(){
-    usernameId = document.getElementById('username').value;
-    passwordId = document.getElementById('password').value;
+    const usernameId = document.getElementById('username').value;
+    const passwordId = document.getElementById('password').value;
 
     if(usernameId == 'admin' && passwordId == 'admin'){
-        document.getElementById('authenticateresult').innerHTML = `Welcome Administrator! Your balance before any transactions today is $${balance}.`;
-        document.getElementById("amountDiv").style.visibility = "visible";
-        document.getElementById('withdrawalbutton').style.visibility = 'visible';
+        document.getElementById('authenticateResult').innerHTML = `Welcome Administrator! Your balance before any transactions today is $${balance}.`;
+        document.getElementById("selection").style.visibility = "visible";
     } else {
-        document.getElementById('authenticateresult').innerHTML = `Username or Password Incorrect!`;
+        document.getElementById('authenticateResult').innerHTML = `Username or Password Incorrect!`;
     }
 }
 
 function showWithdrawalResult(){
-    amountId = document.getElementById('number').value;
+    const amountId = document.getElementById('number').value;
     
-    if (amountId > 0 && amountId <= balance){
-        document.getElementById('withdrawalresult').innerHTML = `You are attempting to withdraw $${amountId}.`;
+    document.getElementById("login").style.visibility = "hidden";
+    document.getElementById("selection").style.visibility = "hidden";
+    document.getElementById("authenticateResult").style.visibility = "hidden";
+    document.getElementById("withdraw").style.visibility = "visible";
+
+    if (amountId === 0 || amountId === ''){
+        document.getElementById('withdrawalResult').innerHTML = '';
+    } else if (amountId > 0 && amountId <= balance){
+        document.getElementById('withdrawalResult').innerHTML = `You are attempting to withdraw $${amountId}.`;
     } else if (amountId > balance){
-        document.getElementById('withdrawalresult').innerHTML = `You are attempting to withdraw more than your balance! Please enter a different amount.`;
+        document.getElementById('withdrawalResult').innerHTML = `You are attempting to withdraw more than your balance! Please enter a different amount.`;
     } else {
-        document.getElementById('withdrawalresult').innerHTML = `Please enter a withdrawal amount greater than zero!`;
+        document.getElementById('withdrawalResult').innerHTML = `Please enter a withdrawal amount greater than zero!`;
     }
 }
