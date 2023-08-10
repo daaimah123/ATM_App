1094d8f194b553a0b95d039f10add78e86bd64d6
Daaimah Tibrey <daaimahtibrey@Daaimahs-Laptop.hsd1.ca.comcast.net>
2023-08-09 18:30:43 -0700

    deposit logic and readme

---
+++ README.md
@@ -0,0 +1,12 @@
+Facts: 
+
+USD banknotes accepted: 1, 2, 5, 10, 20, 50, 100
+<!-- USD banknotes issued: 5, 10, 20, 50, 100 -->
+
+Username: admin
+Password: admin
+
+Future additions:
+- Enhanced UI
+- Banknotes dispursed in increments of 5.
+- [ ] Create a backend server (NodeJS/Express) to handle transactions.
--- js/index.js
+++ js/index.js
@@ -7,6 +7,8 @@ window.onload = function(){
     document.getElementById("finalScreen").style.display = "none";
 }
 
+// To-Do: handle increments of 5s and only distribute 5, 10, 20, 50, and 100 bills
+
 function authenticatedUser(){
     const usernameId = document.getElementById('username').value;
     const passwordId = document.getElementById('password').value;
@@ -20,29 +22,40 @@ function authenticatedUser(){
 }
 
 function withdrawal(){
-    // To-Do: handle increments of 5s and only distribute 5, 10, 20, 50, and 100 bills
     const amountId = document.getElementById('withdrawalNumber').value;
-    
+;
     document.getElementById("authenticate").style.display = "none";
     document.getElementById("selection").style.display = "none";
     document.getElementById("authenticateResult").style.display = "none";
     document.getElementById("withdraw").style.display = "inline-block";
 
     if(amountId > 0 && amountId <= balance){
-        document.getElementById('withdrawalResult').innerHTML = `You are attempting to withdraw $${amountId}.`;
+        document.getElementById('withdrawalResult').innerHTML = `💸 You are attempting to withdraw $${amountId}. Your balance will be $${balance - Number(amountId)} after this transaction.`;
     } else if (amountId > balance){
-        document.getElementById('withdrawalResult').innerHTML = `You are attempting to withdraw more than your balance! Please enter a different amount.`;
+        document.getElementById('withdrawalResult').innerHTML = `⛔️ You are attempting to withdraw more than your balance! Please enter a different amount.`;
     } else {
-        document.getElementById('withdrawalResult').innerHTML = `Please enter a withdrawal amount greater than zero!`;
+        document.getElementById('withdrawalResult').innerHTML = `🧐 Please enter a withdrawal amount greater than zero!`;
     }
 }
 
 function deposit(){
+    const amountId = document.getElementById('depositNumber').value;
+
+    console.log(amountId);
     // To-Do: add any value of bills
     document.getElementById("authenticate").style.display = "none";
     document.getElementById("selection").style.display = "none";
     document.getElementById("authenticateResult").style.display = "none";
     document.getElementById("deposit").style.display = "inline-block";
+    
+    if (amountId > 500){
+        document.getElementById('depositResult').innerHTML = `💰 You are attempting to deposit more than your daily limit! $500 of your funds will be available immediately, making your account balance $${balance + 500}. 🗓️ The other $${Number(amountId) - 500} will take 3-5 business days to post to your account, making your pending balance $${balance + Number(amountId)}.`;
+    } else if (amountId > 0 && amountId < 5000){
+        document.getElementById('depositResult').innerHTML = `🤑 You are depositing $${amountId} to your account. Your available balance will be $${balance + Number(amountId)}.`;
+    } else {
+        document.getElementById('depositResult').innerHTML = `🧐 Please enter a deposit amount greater than zero!`;
+    }
+
 }
 
 // Do I want to check the balance on each screen?
@@ -67,7 +80,9 @@ function backToSect(){
 }
 
 function loginUser(){
-    // TO-DO: handle clearing username and password values
     document.getElementById("finalScreen").style.display = "none";
     document.getElementById("authenticate").style.display = "inline-block";
+    document.getElementById("username").value = '';
+    document.getElementById("password").value = '';
+    // authenticatedUser();
 }
